59a7d956ca0889cf0cde647ec5d3e842a51a4ca2
takku66 <tattaka15@gmail.com>
2021-04-07 00:58:12 +0900

    MyDequeクラス作成することでクリア

--- AizuOJ/ITP2_1_B.java
+++ AizuOJ/ITP2_1_B.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        OnlineJudge pgm = new ITP2_1_A();
+        OnlineJudge pgm = new ITP2_1_B();
         pgm.execute();
     }
 }
@@ -14,38 +14,87 @@ interface OnlineJudge {
     	sc.close();
     }
     abstract void execute(Scanner sc);
+    /**
+     * nextIntよりもnextからのparseの方が早いらしいので
+     * @return
+     */
     public default int next() {
     	return Integer.parseInt(sc.next());
     }
 }
 
-class ITP2_1_A implements OnlineJudge{
-    public void execute(Scanner sc){
-        int max = next();
-        int[] array = new int[max];
-        int arycount = 0;
-        for(int i = 0; i < max; i++) {
-        	int code = next();
+class MyDeque {
+	private int[] array;
+	private int head;
+	private int last;
 
-        	switch(code) {
-        	case 0:
-        		array[arycount] = next();
-        		++arycount;
-        		break;
-        	case 1:
-        		int idx = next();
-        		if(idx > arycount) {
-        			break;
-        		}
-        		System.out.println(array[idx]);
-        		break;
-        	case 2:
-        		array[--arycount] = 0;
-        		break;
-        	default:
-        		break;
-        	}
-        }
-        sc.close();
+	public MyDeque(int max) {
+		this.array = new int[max*2];
+		this.head = max;
+		this.last = max-1;
+	}
+
+	public void addFirst(int e) {
+		--this.head;
+		array[this.head] = e;
+	}
+	public void addLast(int e) {
+		++this.last;
+		array[this.last] = e;
+	}
+	public void removeFirst() {
+		++this.head;
+	}
+	public void removeLast() {
+		--this.last;
+	}
+	public int get(int idx) {
+		return this.array[this.head + idx];
+	}
+
+}
+
+class ITP2_1_B implements OnlineJudge{
+    public void execute(Scanner sc){
+    	int max = next();
+    	MyDeque deque = new MyDeque(max);
+    	for(int i = 0; i < max; i++) {
+    		int code = next();
+    		switch(code) {
+    		case 0:
+    			push(deque, next(), next());
+    			break;
+    		case 1:
+    			System.out.println(deque.get(next()));
+    			break;
+    		case 2:
+    			remove(deque, next());
+    			break;
+    		}
+    	}
+    }
+    private void push(MyDeque dq,int code,  int num) {
+    	switch(code) {
+    	case 0:
+    		dq.addFirst(num);
+    		break;
+    	case 1:
+    		dq.addLast(num);
+    		break;
+		default:
+			break;
+    	}
+    }
+    private void remove(MyDeque dq,int code) {
+    	switch(code) {
+    	case 0:
+    		dq.removeFirst();
+    		break;
+    	case 1:
+    		dq.removeLast();
+    		break;
+		default:
+			break;
+    	}
     }
 }
